79e84b8e77cb460879baa4e868388212ac586adb
Samuel <samuel@umich.edu>
2019-04-06 18:10:51 -0400

    stroke vae successful

--- vae/vae.py
+++ vae/vae.py
@@ -14,15 +14,18 @@ import numpy as np
 import imageio
 
 BATCH_SIZE = 64
-NB_UPDATES = 1001
-CONCISENESS = 100
-LEARN_RATE = 0.01
+NB_UPDATES = 2001
+CONCISENESS = 50
+LEARN_RATE = 0.001
 
-DATA_DIM = 28*28 
+H = 28 
+W = 28
+NB_SEGS = 6
+DATA_DIM = H*W
 LATENT_DIM = 20
 NOISE_DIM = LATENT_DIM
 LATENT_DISTR_DIM = 2*LATENT_DIM
-DATA_DISTR_DIM = 2*DATA_DIM
+DATA_DISTR_DIM = NB_SEGS*5
 
 DATA_SHAPE = [BATCH_SIZE, DATA_DIM] 
 LATENT_NOISE_SHAPE = [BATCH_SIZE, NOISE_DIM] 
@@ -30,6 +33,8 @@ DATA_NOISE_SHAPE = [BATCH_SIZE, DATA_DIM]
 
 def lrelu(layer):
     return tf.maximum(0.2*layer, layer)
+def clip(layer):
+    return tf.maximum(0.0, tf.minimum(1.0, layer))
 
 class Encoder(object):
     def __init__(self, name='enc', HIDDEN_DIM=100):
@@ -51,13 +56,35 @@ class Decoder(object):
                 initializer=tf.contrib.layers.xavier_initializer())
         self.weightb = tf.get_variable('%s-weightb'%name, shape=[HIDDEN_DIM, DATA_DISTR_DIM], dtype=tf.float32,
                 initializer=tf.contrib.layers.xavier_initializer())
+        self.weightc = tf.get_variable('%s-weightc'%name, shape=[DATA_DIM], dtype=tf.float32,
+                initializer=tf.random_normal_initializer(stddev=0.01))
 
     def __call__(self, latent): 
         hidden = lrelu(tf.matmul(latent, self.weighta))
         out = tf.matmul(hidden, self.weightb)
-        means = tf.sigmoid(out[: , :DATA_DIM])
-        stdvs = tf.exp(out[: , DATA_DIM:]) + 0.005
+
+        s = 0.1 + 0.1 * tf.exp(10.0*self.weightc)
+        stdvs = tf.stack([s]*BATCH_SIZE, axis=0)
+
+        line_segs = tf.reshape(out[: , :NB_SEGS*5], [-1, NB_SEGS, 5])
+        ys = clip(0.5 + 0.5*tf.expand_dims(tf.expand_dims(line_segs[: , : , 0], axis=2), axis=2))
+        xs = clip(0.5 + 0.5*tf.expand_dims(tf.expand_dims(line_segs[: , : , 1], axis=2), axis=2))
+        ye = clip(0.5 + 0.5*tf.expand_dims(tf.expand_dims(line_segs[: , : , 2], axis=2), axis=2))
+        xe = clip(0.5 + 0.5*tf.expand_dims(tf.expand_dims(line_segs[: , : , 3], axis=2), axis=2))
+        t  = 0.02 + 0.2 * clip(tf.expand_dims(tf.expand_dims(line_segs[: , : , 4], axis=2), axis=2))
+
+        yc = (1.0/H) * tf.constant(np.expand_dims(np.expand_dims(np.expand_dims(np.arange(float(H)), axis=1), axis=0), axis=0), dtype=tf.float32)
+        xc = (1.0/W) * tf.constant(np.expand_dims(np.expand_dims(np.expand_dims(np.arange(float(W)), axis=0), axis=0), axis=0), dtype=tf.float32)
+
+        means = tf.reduce_max((
+                tf.sqrt(1e-4 + tf.square(ys-ye) + tf.square(xs-xe))
+               -tf.sqrt(1e-4 + tf.square(ys-yc) + tf.square(xs-xc))
+               -tf.sqrt(1e-4 + tf.square(yc-ye) + tf.square(xc-xe))
+        )/t, axis=1)
+        means = tf.reshape(tf.exp(means), [-1, DATA_DIM])
+
         return tf.concat([means, stdvs], axis=1) 
+
 
 def latent_sampler(noise, latent_distr):
     return latent_distr[: , :LATENT_DIM] + tf.multiply(noise, latent_distr[: , LATENT_DIM:])  
@@ -153,11 +180,13 @@ with tf.Session() as sess:
             d = get_data_noise()
             r, s = sess.run([V.recon_distribution[0,:DATA_DIM],
                              V.recon_distribution[0,DATA_DIM:]], feed_dict={V.latent_noise:n, V.data:b, V.data_noise:d})
+            d = np.maximum(0, np.minimum(1, 0.1*np.abs(r-b[0])/s))
             r = np.maximum(0, np.minimum(1, r))
             s = np.maximum(0, np.minimum(1, s))
             side_by_side = np.concatenate([
-                np.reshape(b[0], (28, 28)), 
-                np.reshape(r, (28, 28)),
-                np.reshape(s, (28, 28))
+                np.reshape(b[0], (H, W)), 
+                np.reshape(r, (H, W)),
+                np.reshape(s, (H, W)),
+                np.reshape(d, (H, W))
                 ], axis=1) 
             imageio.imwrite('r%04d.png'%i, (255*side_by_side).astype(np.uint8))
